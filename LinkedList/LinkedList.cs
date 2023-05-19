c26cdd0675915ced333672723276969b0d98385c
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-05-20 01:16:12 +0530

    [Pankaj] To create simple linked list of 56,30 and 70.

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        private Node head;
+        //To insert
+        public void Add(int value)
+        {
+            Node newNode = new Node(value);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+        // To display
+        public void Display()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.Value + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList myList = new LinkedList();
+            //To add
+            myList.Add(56);
+            myList.Add(30);
+            myList.Add(70);
+            //To display
+            myList.Display();
+        }
+
+    }
+}
+
